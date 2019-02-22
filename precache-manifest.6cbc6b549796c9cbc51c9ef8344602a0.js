604161710fe68cefc7b67a8b94446c1d19cd1a93
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-02-22 13:01:29 +0200

    Updates

---
+++ precache-manifest.6cbc6b549796c9cbc51c9ef8344602a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75e0ea0e0636e34a8c45",
+    "url": "/tictactoe-react/static/css/main.034f21c8.chunk.css"
+  },
+  {
+    "revision": "75e0ea0e0636e34a8c45",
+    "url": "/tictactoe-react/static/js/main.75e0ea0e.chunk.js"
+  },
+  {
+    "revision": "2e4160c1d6ee9dec8bb8",
+    "url": "/tictactoe-react/static/js/runtime~main.2e4160c1.js"
+  },
+  {
+    "revision": "6ddb13c3dc97a829a451",
+    "url": "/tictactoe-react/static/js/2.6ddb13c3.chunk.js"
+  },
+  {
+    "revision": "2edbc620036c2bd65ef7b44aae5d82f5",
+    "url": "/tictactoe-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tictactoe-react/precache-manifest.f7b75a5ae1c543c1dcc46649ca3d822e.js"
+  "/tictactoe-react/precache-manifest.6cbc6b549796c9cbc51c9ef8344602a0.js"
 );
 
 workbox.clientsClaim();
